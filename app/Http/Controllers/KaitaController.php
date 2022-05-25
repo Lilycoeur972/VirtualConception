5ecdcbad596aaa1e18d3e0af87f0dcc8717eb7be
Steph-Hill <stephen_hillion@outlook.fr>
2022-05-25 08:55:05 -0400

    stephen modif

--- app/Http/Controllers/KaitaController.php
+++ app/Http/Controllers/KaitaController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class KaitaController extends Controller
 {
     public function index(){
 
+        $posts= User::all();
         return back() ;
     }
 }
