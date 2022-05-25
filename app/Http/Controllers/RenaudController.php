8ef260ebc8567c235f279edf419a8a88f1b62a7c
oOoRenaudoOo <salliotrenaud@gmail.com>
2022-05-25 08:38:25 -0400

    Renaud

---
+++ app/Http/Controllers/RenaudController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RenaudController extends Controller
+{
+    public function index() {
+
+        // return view();
+
+    }
+}
